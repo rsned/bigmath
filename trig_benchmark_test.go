087a659561d7e4db0a3a21e305aaf5a69f981219
Robert Snedegar <viking@gmail.com>
2025-07-08 07:53:22 -0700

    Add license header to each file.

--- trig_benchmark_test.go
+++ trig_benchmark_test.go
@@ -1,3 +1,17 @@
+// Copyright 2025 Robert Snedegar
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package bigmath
 
 import (
@@ -96,8 +110,6 @@ func BenchmarkTrigPrecisionScaling(b *testing.B) {
 
 // BenchmarkTrigCommonInputValues runs the trig methods against a range of common input values.
 func BenchmarkTrigCommonInputValues(b *testing.B) {
-	//testValues := []float64{0.1, 1.0, math.Pi / 4, math.Pi / 3, math.Pi / 2, math.Pi, 2 * math.Pi}
-
 	for _, val := range specialTrigValues {
 		x := big.NewFloat(val)
 		x.SetPrec(256)
